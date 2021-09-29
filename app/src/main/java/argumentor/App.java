9a4194bac25215d7503babd0313ddfdc7bdcc73f
aejmmark <aejmelaeus.markus@gmail.com>
2021-09-29 14:55:22 +0300

    added ui

--- app/src/main/java/argumentor/App.java
+++ app/src/main/java/argumentor/App.java
@@ -1,5 +1,7 @@
 package argumentor;
 
+import java.util.Scanner;
+
 public class App {
 
     /**
@@ -8,8 +10,19 @@ public class App {
     public static void main(String[] args) {
         Tree tree = new Tree();
         Functions func = new Functions();
+        Scanner scn = new Scanner(System.in);
         func.processData(tree);
-        String result = func.generate(tree);
-        System.out.println(result);
+        System.out.println("Greetings! Type something to gain enlightenment. EXIT leaves the app.");
+        while (true) {
+            String input = scn.nextLine();
+            if (input.equals("EXIT")) {
+                break;
+            } else {
+                String result = func.generate(tree);
+                System.out.println(result);
+            }
+        }
+        System.out.println("Farewell");
+        scn.close();
     }
 }
