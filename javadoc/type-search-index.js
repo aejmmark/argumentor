a2915955962bccd0d48a105afab5ca0e0096ad06
aejmmark <aejmelaeus.markus@gmail.com>
2021-09-30 14:41:49 +0300

    Remove duplicate javadoc dir

--- javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"argumentor","l":"App"},{"p":"argumentor","l":"Edge"},{"p":"argumentor","l":"Functions"},{"p":"argumentor","l":"Word"}]
